060052bbc072bd6a149752a2c23104d565d13f5c
ymkd <nobuhiro.yamakado@gmail.com>
2022-12-19 02:58:39 +0900

    add ls and export

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,6 +4,10 @@
 # a nonzero exit code.
 set -e
 
+ls -la
+
+export
+
 # A command or script succeeded.
 echo "A command or script was successful."
 exit 0
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,3 +5,11 @@
 #
 #  Created by quiver on 2022/12/19.
 #  
+
+ls -la
+
+export
+
+# A command or script succeeded.
+echo "A command or script was successful."
+exit 0
